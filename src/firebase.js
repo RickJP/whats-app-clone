4cfdc4d4da0f709c3bf35091244980776b2396fb
rickjp <3741326+RickJP@users.noreply.github.com>
2020-09-05 08:43:46 +0900

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDk0CvTE-VZSdMvg-ylqjUb6TQ2XH9GJF0',
+  authDomain: 'whatsapp-82e08.firebaseapp.com',
+  databaseURL: 'https://whatsapp-82e08.firebaseio.com',
+  projectId: 'whatsapp-82e08',
+  storageBucket: 'whatsapp-82e08.appspot.com',
+  messagingSenderId: '382113216671',
+  appId: '1:382113216671:web:8765e14a90580b98f94aaa',
+  measurementId: 'G-QMJ7MWCMKK',
+};
